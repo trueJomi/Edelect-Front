873b3237c823ed7fb6340d3b96aedac93f270b3e
Diego Renato Torres Añorga <diegot14321@gmail.com>
2021-12-11 00:50:40 -0500

    avance carpetas test

--- src/app/admin/tests/shared/test.service.ts
+++ src/app/admin/tests/shared/test.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../../environments/environment";
+import {Test} from "./test.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TestService {
+  private apiBase:string=environment.apiBase;
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  getAllTest(){
+    return this.http.get<Test[]>('${this.apiBase}/test');
+  }
 }
