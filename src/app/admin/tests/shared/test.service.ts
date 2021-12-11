fb6552e5b0c58123a2c766d8f0d640cf41080211
Pixgel <pixgel13040@gmail.com>
2021-12-11 08:30:44 -0500

    test Services

--- src/app/admin/tests/shared/test.service.ts
+++ src/app/admin/tests/shared/test.service.ts
@@ -1,16 +1,36 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../../environments/environment";
-import {Test} from "./test.model";
+import {Carrera, Test} from "./test.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TestService {
+
+  private controller:string='/test'
   private apiBase:string=environment.apiBase;
   constructor(private http:HttpClient) { }
 
   getAllTest(){
-    return this.http.get<Test[]>('${this.apiBase}/test');
+    return this.http.get<Test[]>(`${this.apiBase}${this.controller}`);
+  }
+  crearTestBase(test:Test){
+    return this.http.post<Test>(`${this.apiBase}${this.controller}/base`,test);
+  }
+  BuscarTestByUser(user:string){
+    return this.http.get<Test[]>(`${this.apiBase}${this.controller}/user/${user}`);
+  }
+  editTestBase(test:Test){
+    return this.http.put<Test>(`${this.apiBase}${this.controller}/base`,test);
+  }
+  BuscarTestById(id:number){
+    return this.http.get<Test>(`${this.apiBase}${this.controller}/id/${id}`);
+  }
+  ObtenerResultados(id:number){
+    return this.http.get<string>(`${this.apiBase}${this.controller}/resultados/${id}`);
+  }
+  CarrerasRelacionadas(id:number){
+    return this.http.get<Carrera[]>(`${this.apiBase}${this.controller}/relacionado/${id}`);
   }
 }
--- src/app/auth/shared/usuario.model.ts
+++ src/app/auth/shared/usuario.model.ts
@@ -1,11 +1,7 @@
-
-
 export class Usuario{
     idUsuario?: number;
     userName?: string;
     nombre?: string;
     correo?: string;
     contrasena?: string;
-    coment?:Array<any>
-    tests?:Array<any>
 }
